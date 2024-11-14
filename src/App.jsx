2f5574abec8d7083d37358cd886e59d9e6dec730
riccog <riccogoss@gmail.com>
2024-11-14 15:41:31 -0500

    modified the configuration to access custos instead of keycloak

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,35 @@ import './App.css';
 import RouteList from './Routes.jsx';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+async function fetchData() {
+  try{
+    const response = await fetch("https://api.playground.usecustos.org/api/v1/identity-management/tenant/10000000/.well-known/openid-configuration")
+    const data = await response.json();
+    console.log(data)
+    return data;
+  } catch(error) {
+    console.error(error);
+  }
+}
+
+const custosInfo = await fetchData();
+console.log(custosInfo.token_endpoint);
+
 
 const configuration = {
   client_id: 'custos-u8fth4tahwfil3ue3vpm-10000000',
   redirect_uri: 'http://localhost:5173/callback',
   scope: 'openid profile email',
-  authority: 'https://authz.playground.usecustos.org/realms/10000000',
+  authority: 'https://api.playground.usecustos.org/api/v1/identity-management/tenant/10000000/.well-known/openid-configuration',
+  authority_configuration: {
+    authorization_endpoint: custosInfo.authorization_endpoint,
+    token_endpoint: custosInfo.token_endpoint,
+    userinfo_endpoint: custosInfo.userinfo_endpoint,
+    issuer: 'https://authz.playground.usecustos.org/realms/10000000',
+  },
   response_type: 'code',
-  state: 'active'
+  state: 'active',
+  loadUserInfo: true,
 };
 
 function App() {
--- src/pages/WelcomeScreen.jsx
+++ src/pages/WelcomeScreen.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import { OidcSecure } from '@axa-fr/react-oidc';
 import { useOidcUser, useOidcIdToken, useOidcAccessToken } from '@axa-fr/react-oidc';
 import { useEffect } from 'react';
-import UserInfo from '../UserInfo.jsx';
-
-
 
 
 
@@ -12,28 +9,21 @@ const WelcomeScreen = () => {
   const {oidcUser, oidcUserLoadingState} = useOidcUser();
   const {idToken, idTokenPayload} = useOidcIdToken();
   const {accessToken, accessTokenPayload} = useOidcAccessToken();
-
-  useEffect(() => {
-    const fetchUserInfo = async () => {
-      console.log("access token", accessToken);
-      if (accessToken) {
-        response = await UserInfo(accessToken);
-        console.log("userInfo: Response:", reponse);
-      } else {
-        console.log('waiting');
-      }
-    };
-
-    fetchUserInfo(); // Call the async function
-
-  }, [accessToken]);
-
+  
     return (
       <OidcSecure>
-        <h1>Welcome{oidcUser ? `, ${oidcUser.given_name}` : ''}</h1>
+        <h1>Welcome{oidcUser ? `, ${oidcUser.name}` : ''}</h1>
         {oidcUser && (
           <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
             <p>Email: {oidcUser.email}</p>
+            <br />
+            <button onClick={() => {
+              if (oidcUser.groups.includes("TASK_ADMIN_GRP1")) {
+                console.log("You are part of the group")
+              } else {
+                console.log("You are not part of the group")
+              }
+            }}>Click Me!</button>
           </div>
         )}
         {<p className="card-text">{JSON.stringify(accessToken)}</p>}
